2395a8fddae929e186329ec816c440e7efeb366c
cohenchris <cohen50@purdue.edu>
2021-02-10 22:20:06 -0500

    added ReactJS component for privacy badge

---
+++ components/PrivacyBadge.js
@@ -0,0 +1,29 @@
+// Made by Chris Cohen
+// <PrivacyBadge service="facebook"/>
+
+import axios from 'axios';
+import { useState } from 'react';
+
+function PrivacyBadge(props) {
+  const [id, setID] = useState("")
+
+  // GET https://tosdr.org/api/v1/service/<props.service> and parse ID from JSON
+  let url = "https://tosdr.org/api/v1/service/" + props.service
+  axios.get(url)
+    .then(data => setID(data.data.id))
+
+  // Display badge, around which is a link to the ToS;DR webpage for that service
+  //       badge    - https://tosdr.org/api/badge/en_<id>.svg
+  //       ToS page - https://tosdr.org/en/service/<id>
+
+  let service_url = "https://tosdr.org/en/service/" + id
+  let badge = "https://tosdr.org/api/badge/en_" + id + ".svg"
+
+  return (
+      <a href={service_url}>
+        <img src={badge}/>
+      </a>
+  );
+}
+
+export default PrivacyBadge;
