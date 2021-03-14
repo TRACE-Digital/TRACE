747a45c4b559560f419848c0ebe517c1179b4039
cohenchris <cohen50@purdue.edu>
2021-03-14 15:21:01 -0400

    added in firstName and lastName support on the frontend

--- src/views/Search.js
+++ src/views/Search.js
@@ -27,6 +27,8 @@ function SearchComponent() {
   const [keywordsEntered, setKeywordsEntered] = useState(false);
   const [tagsEntered, setTagsEntered] = useState(false);
   const [userNames, setUserNames] = useState([]);
+  const [firstNames, setFirstNames] = useState([]);
+  const [lastNames, setLastNames] = useState([]);
   const [resultIds, setResultsIds] = useState([]);
   const [progress, setProgress] = useState(-1);
   const [categories, setCategories] = useState(tags.slice());
@@ -52,10 +54,26 @@ function SearchComponent() {
 
   // Function to handle keypresses for first and last name refine search functionality
   function nameKeyPress(e) {
-    // TODO
+    if (e.keyCode === 13) { // keycode for Enter
+      if (e.target.id === "firstName") {  // Handle enter for firstName input
+        if (!firstNames.includes(e.target.value) && e.target.value !== '') {
+          firstNames.push(e.target.value);
+          setFirstNames([...firstNames]);
+        }
+        e.target.value = '';
+      }
+      else if (e.target.id === "lastName") {  // Handle enter for last name input
+        if (!lastNames.includes(e.target.value) && e.target.value !== '') {
+          lastNames.push(e.target.value);
+          setLastNames([...lastNames]);
+        }
+        e.target.value = '';  
+      }
+    }
   }
 
   function keyPress(e) {
+    console.log("testing!")
     if (e.keyCode === 13) {
       if (!userNames.includes(e.target.value) && e.target.value !== '') {
         userNames.push(e.target.value);
@@ -93,7 +111,12 @@ function SearchComponent() {
 
       const searchDef = new SearchDefinition(undefined, taggedSiteNames);
       searchDef.userNames = userNames;
+      // TODO: add firstNames and lastNames
+      searchDef.firstNames = firstNames;
+      searchDef.lastNames = lastNames;
       await searchDef.save();
+
+      // TODO: deal with firstName and lastName stuff received?
 
       const search = await searchDef.new();
 
@@ -111,12 +134,24 @@ function SearchComponent() {
     setUserNames([...userNames]);
   }
 
+  function deleteNameEntry(entry, type) {
+    if (type === "first") {
+      firstNames.splice(firstNames.indexOf(entry), 1)
+      setFirstNames([...firstNames])
+    }
+    else if (type === "last") {
+      lastNames.splice(lastNames.indexOf(entry), 1)
+      setLastNames([...lastNames])    }
+    else {
+      console.error("Invalid deleteNameEntry entry type!")
+    }
+  }
+
   function typing() {
     setKeywordsEntered(false);
   }
 
   function handleClickCheckbox(e) {
-
     if (categories.includes(e.target.value)) {
       categories.splice(categories.indexOf(e.target.value), 1);
       setCategories([...categories]);
@@ -202,10 +237,10 @@ function SearchComponent() {
       <div className="search-info">Find your digital footprint. Manage your online presence. Our service allows you to increase your social media engagement while keeping your privacy a priority. Sync your information or work locally.</div>
 
       <div className="one">
-        <div className="three">{userNames.map(item => <div className="entered" key={item}>
+        <div className="three">
+          {userNames.map(item => <div className="entered" key={item}>
           <i className="icon fas fa-times" onClick={() => deleteEntry(item)}></i>
-          {item}
-        </div>)}
+          {item}</div>)}
         </div>
         <div className="two">
           <input
@@ -248,52 +283,48 @@ function SearchComponent() {
         <br/>
         <br/>
         <br/>
-        {/* First Name section of refine search dropdown goes here*/}
         <h1>NAMES</h1>
-        {/* 
-        <div className="one">
-        <div className="three">{userNames.map(item => <div className="entered" key={item}>
-          <i className="icon fas fa-times" onClick={() => deleteEntry(item)}></i>
-          {item}
-        </div>)}
-        </div>
-        <div className="two">
-          <input
-            id="search-bar"
-            className="two-search"
-            placeholder="enter keyword"
-            onKeyDown={keyPress}
-            onChange={typing}>
-          </input>
-        </div>
-        <div className="four">
-          <i onClick={submitSearch} className="fas fa-search"></i>
-        </div>
-      </div> 
-      */}
-      {/* X BUTTON -           <i className="icon fas fa-times" onClick={() => deleteEntry(item)}></i> */}
-      {/* HANDLE KEYPRESS - nameKeyPress */}
-      <div className="">
-        <div className="">{userNames.map(item => <div className="entered" key={item}>
-          <i className="icon fas fa-times" onClick={() => deleteEntry(item)}></i>
-          {item}
-        </div>)}
-        </div>
-        <div className="">
-          <input
-            id=""
-            className=""
-            placeholder="enter first name / nickname"
-            onKeyDown={nameKeyPress}>
-            {/* onChange={typing}> TODO: what does this do??*/}
-          </input>
-        </div>
-      </div> 
+        {/* First Name section for refine search dropdown goes here*/}
+        <h6>Enter First Names / Nicknames:</h6>
+        {/* Text box input */}
+        <Row className="name" id="firstName">
+          <div className="nameEntryInput" id="firstName">
+            <input
+              id="firstName"
+              placeholder="first name/nickname"
+              onKeyDown={nameKeyPress}>
+            </input>
+          </div>
+          {/* Currently entered firstNames */}
+          <Col className="nameList">
+            {firstNames.map(item => <div className="entered" key={item}>
+              <i className="icon fas fa-times" onClick={() => deleteNameEntry(item, "first")}></i>
+              {item}</div>)}
+          </Col>
+        </Row>
+        <br/>
 
-      {/* Last Name section of refine search dropdown goes here*/}
-
-
+        {/* Last Name section for refine search dropdown goes here */}
+        <h6>Enter Last Names / Maiden Names:</h6>
+        {/* Text box input */}
+        <Row className="name" id="lastName">
+          <div className="nameEntryInput" id="lastName">
+            <input
+              id="lastName"
+              placeholder="last name/maiden name"
+              onKeyDown={nameKeyPress}>
+            </input>
+          </div>
+          {/* Currently entered lastNames */}
+          <Col className="nameList">
+            {lastNames.map(item => <div className="entered" key={item}>
+              <i className="icon fas fa-times" onClick={() => deleteNameEntry(item, "last")}></i>
+              {item}</div>)}
+          </Col>
+        </Row>
+        <br/>
       </div>
+
       <div className={keywordsEntered ? "error-message-visible" : (tagsEntered ? "error-message-visible" : "error-not-visible")}>
         {keywordsEntered ? "please enter at least one keyword" : (tagsEntered ? "please enter at least one tag" : "")}
       </div>
