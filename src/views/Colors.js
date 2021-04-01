a8095df95f5efc2632a630eef0ddd34f6215280a
isabelbattag <isabelbattag@gmail.com>
2021-03-31 20:39:42 -0400

    most recent work

--- src/views/Colors.js
+++ src/views/Colors.js
@@ -1,13 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { Row, Col } from "reactstrap";
-import { AlphaPicker, ChromePicker, CirclePicker, CompactPicker, SketchPicker, SliderPicker, TwitterPicker } from 'react-color'
+import React, {useState, useEffect} from 'react';
+import {Row, Col} from "reactstrap";
+import {ChromePicker} from 'react-color'
+import {Button, ButtonGroup} from "reactstrap";
+import classNames from "classnames";
+import {ThirdPartyAccount, accounts} from "trace-search";
+
 
 function Colors(props) {
+    
 
     const [titleColor, setTitleColor] = useState();
-    const [backgroundColor, setBackgroundColor] = useState();
+    const [backColor, setBackgroundColor] = useState();
     const [siteColor, setSiteColor] = useState();
     const [iconColor, setIconColor] = useState();
+    const [chromeColor, setChromeColor] = useState();
+    const [currentButton, setCurrentButton] = useState("Title");
+    const [claimedAccounts, setClaimedAccounts] = useState({});
+
+    const [showThemeFeature, setThemeFeature] = useState(false);
+    const [showSiteFeature, setSiteFeature] = useState(true);
 
     const [colorProps, setColorProps] = useState([{
         "titleColor":"#FFFFFF",
@@ -16,97 +27,176 @@ function Colors(props) {
         "iconColor":"Default"
     }])
 
-    function handleTitleChange(colors) {
-        setTitleColor(colors);
-        colorProps[0].titleColor = colors.hex;
-        props.onSelectLanguage(colorProps);
+    /**
+     * Monitor for new claimed accounts. Every time the accounts array is updated, re-render to show the proper tiles.
+     */
+    useEffect(() => {
+        const loadAccounts = async () => {
+        try {
+            // Load all accounts from the database into memory
+            await ThirdPartyAccount.loadAll();
+            // setPlsRender((current) => !current);
+        } catch (e) {
+            console.error("Failed to load accounts from the database!");
+            console.error(e);
+            return {};
+        }
+        return accounts;
+        };
+
+        loadAccounts().then(() => {
+        setClaimedAccounts(accounts);
+        });
+
+    }, [accounts]);
+
+    function handleColorPicker(e){
+        setChromeColor(e);
+        console.log(currentButton);
+        if (currentButton=="Title"){
+            setTitleColor(e.hex);
+            colorProps[0].titleColor = e.hex;
+            props.onSelectLanguage(colorProps);
+        }
+        else if (currentButton=="Site"){
+            setSiteColor(e.hex);
+            colorProps[0].siteColor = e.hex;
+            props.onSelectLanguage(colorProps);
+        }
+        else if (currentButton=="Icon"){
+            setIconColor(e.hex);
+            colorProps[0].iconColor = e.hex;
+            props.onSelectLanguage(colorProps);
+        }
+        else if (currentButton=="Background"){
+            setBackgroundColor(e.hex);
+            colorProps[0].backgroundColor = e.hex;
+            props.onSelectLanguage(colorProps);
+        }
     }
 
-    function handleBackgroundChange(colors) {
-        setBackgroundColor(colors);
-        colorProps[0].backgroundColor = colors.hex;
-        props.onSelectLanguage(colorProps);
+    function handleClick(e){
+        console.log(e.target.id);
+        if (e.target.id=="title"){
+            setCurrentButton("Title");
+            
+        }
+        else if (e.target.id=="site"){
+            setCurrentButton("Site");
+        }
+        else if (e.target.id=="background"){
+            setCurrentButton("Background");
+        }
+        else if (e.target.id=="icon"){
+            setCurrentButton("Icon");
+        }
     }
 
-    function handleSiteChange(colors) {
-        setSiteColor(colors);
-        colorProps[0].siteColor = colors.hex;
-        props.onSelectLanguage(colorProps);
+    function handleSite(e){
+        setThemeFeature(false);
+        setSiteFeature(true);
     }
 
-    function handleIconChange(e) {
-        setIconColor(e.hex);
-        colorProps[0].iconColor = e.hex;
-        props.onSelectLanguage(colorProps);
+    function handleThemes(e){
+        setThemeFeature(true);
+        setSiteFeature(false);
     }
+
+    function handleOver(e){
+        e.target.style.backgroundColor = "revert";
+  
+        console.log("IN HEREEEEE");
+    }
+
 
   return (
     <>
       <div className="popup">
-            <div className="title">Themes</div>
-                <div className="colors">
-                    <Row>
-                        <Col>
-                            <div className="color-div">
-                                <div className ="popup-titles">Title</div>
-                                <ChromePicker color={titleColor} disableAlpha onChangeComplete={handleTitleChange} />
-                            </div>
-                        </Col>
-                        <Col>
-                            <div className="color-div">
-                                <div className ="popup-titles">Background</div>
-                                <ChromePicker color={backgroundColor} disableAlpha onChangeComplete={ handleBackgroundChange } />
-                            </div>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <div className="color-div">
-                                <div className ="popup-titles">Site Cards</div>
-                                <ChromePicker color={siteColor} disableAlpha onChangeComplete={ handleSiteChange } />
-                            </div>
-                        </Col>
-                        <Col>
-                            <div className="color-div">
-                                <div className ="popup-titles">Icons</div>
-                                <ChromePicker color={iconColor} disableAlpha onChangeComplete={ handleIconChange } />
-                            </div>
-                        </Col>
-                    </Row>
+           <div className="icon-wrap">
+                <i className="tim-icons icon-simple-remove icon" onClick={props.closePopup}>
+                </i>
+            </div>
+
+                <div className="colors" style={showSiteFeature ? {margin:"0px"} : null}>
+                    <div className="toggle-containers" style={showSiteFeature ? {padding:"5px"} : null}>
+                    <ButtonGroup
+                        className="btn-group-toggle"
+                        data-toggle="buttons"
+                    >
+                        <Button
+                            tag="label"
+                            color="info"
+                            id="0"
+                            size="lg"
+                            className={classNames("btn-neutral")}
+                            onClick={handleSite}
+                        >
+                            <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                                Edit Sites
+                            </span>
+                        </Button>
+                        <Button
+                            color="info"
+                            id="1"
+                            size="lg"
+                            tag="label"
+                            className={classNames("btn-neutral")}
+                            onClick={handleThemes}
+                        >
+                            <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                                Edit Themes
+                            </span>
+                        </Button>
+                    </ButtonGroup>
+
+                {/* Theme Feature */}
                 </div>
-                {/* <div>
-                    <ul className="editor">
-                        <img id = "Default" className={isDefaultSelected ? "color-choices-selected" : "color-choices"} src={require("assets/img/Default.png").default} onClick={handleClick}/>
-                        <img id = "Blue" className={isBlueSelected ? "color-choices-selected" : "color-choices"} src={require("assets/img/Blue.png").default} onClick={handleClick}/>
-                        <img id = "Purple" className={isPurpleSelected ? "color-choices-selected" : "color-choices"} src={require("assets/img/Purple.png").default} onClick={handleClick}/>
-                        <img id = "Pink" className={isPinkSelected ? "color-choices-selected" : "color-choices"} src={require("assets/img/Pink.png").default} onClick={handleClick}/>
-                    </ul>
-                </div> */}
-                
-                {/* <div className="title">Add To Profile</div>
-                    <div className="new-sites">
-                        <ul className="new-sites-ul">
-                                <li className = "new-sites-ul">Site 1</li>
-                                <li className = "new-sites-ul">Site 2</li>
-                                <li className = "new-sites-ul">Site 3</li>
-                                <li className = "new-sites-ul">Site 4</li>
-                                <li className = "new-sites-ul">Site 5</li>
-                                <li className = "new-sites-ul">Site 6</li>
-                                <li className = "new-sites-ul">Site 7</li>
-                                <li className = "new-sites-ul">Site 8</li>
-                                <li className = "new-sites-ul">Site 9</li>
-                                <li className = "new-sites-ul">Site 10</li>
-                                <li className = "new-sites-ul">Site 2</li>
-                                <li className = "new-sites-ul">Site 3</li>
-                                <li className = "new-sites-ul">Site 4</li>
-                                <li className = "new-sites-ul">Site 5</li>
-                                <li className = "new-sites-ul">Site 6</li>
-                                <li className = "new-sites-ul">Site 7</li>
-                                <li className = "new-sites-ul">Site 8</li>
-                                <li className = "new-sites-ul">Site 9</li>
-                                <li className = "new-sites-ul">Site 10</li>
-                        </ul>
-                    </div> */}
+                    {showThemeFeature ?
+                        <Row>
+                            <Col>
+                                <div className="options">
+                                    <div id="title" className="myButton" style={currentButton=="Title"? {backgroundColor: `${titleColor}`,color:"white",opacity:1, border: "3px solid #dadada"} : {backgroundColor: `${titleColor}`,color:"white", opacity:.7}} onClick={handleClick}>
+                                        Title Color
+                                    </div>
+                                    <div id="background" className="myButton" style={currentButton=="Background"? {backgroundColor: `${backColor}`,color:"white",opacity:1, border: "2px solid #dadada"}: {backgroundColor: `${backColor}`,color:"white", opacity:.7}} onClick={handleClick}>
+                                        Background Color
+                                    </div>
+                                    <div id="site" className="myButton" style={currentButton=="Site"? {backgroundColor: `${siteColor}`,color:"white",opacity:1, border: "2px solid #dadada"} : {backgroundColor: `${siteColor}`,color:"white", opacity:.7}} onClick={handleClick}>
+                                        Site Color
+                                    </div>
+                                    <div id="icon" className="myButton" style={currentButton=="Icon"? {backgroundColor: `${iconColor}`,color:"white",opacity:1, border: "2px solid #dadada"} : {backgroundColor: `${iconColor}`,color:"white", opacity:.7}} onClick={handleClick}>
+                                        Icon Color
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col>
+                                <ChromePicker color={chromeColor} disableAlpha onChangeComplete={handleColorPicker} />
+                            </Col>
+                        </Row>
+                        : null
+                    }
+                </div>
+
+                {/* Site Feature */}
+                <div className="add-site">
+                    {showSiteFeature ?
+                        <div className="sContainer">
+                            <table className="siteTable">
+                                {Object.values(claimedAccounts).map(item => (
+                                    <tr className="siteTr">
+                                        <td className="siteTd">
+                                            <div className="add-remove-button">Add</div>
+                                            <span className="site-info">{item.site.name}</span>: {item.userName}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </table>
+                        </div>
+                        : null
+                    }
+                    
+                </div>
+          
             </div>
         </>
     );
--- src/views/Editor.js
+++ src/views/Editor.js
@@ -1,28 +1,18 @@
 import React, { useState, useEffect } from "react";
 import Colors from "views/Colors.js";
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import { Auth } from "aws-amplify";
 import { ThirdPartyAccount, accounts, AccountType } from "trace-search";
 import SiteCard from "components/SiteCard/SiteCard";
-import {
-  GridContextProvider,
-  GridDropZone,
-  GridItem,
-  swap
-} from "react-grid-dnd";
-
-// reactstrap components
-import {
-  Card,
-  CardBody,
-  Row,
-  Col,
-} from "reactstrap";
+import { EditText, EditTextarea } from 'react-edit-text';
+import {GridContextProvider, GridDropZone, GridItem, swap} from "react-grid-dnd";
+import { Link } from "react-router-dom";
+import {Row, Col} from "reactstrap";
 
 var name = "Isabel Battaglioli";
 
+
 const Editor = () => {
   const [claimedAccounts, setClaimedAccounts] = useState({});
+  const[title, setTitle] = useState("Enter Title");
   const [isOpen, setIsOpen] = useState(false);
   const [colorScheme, setColorScheme] = useState([{
     "titleColor":"#FFFFFF",
@@ -49,11 +39,18 @@ const Editor = () => {
   function handleLanguage(colorChoice){
     const updated = [...colorChoice];
     setColorScheme([...updated]);
-    console.log(colorScheme[0].titleColor);
-    console.log(colorScheme[0].siteColor);
-    console.log(colorScheme[0].backgroundColor);
-    console.log(colorScheme[0].iconColor);
-    console.log(colorScheme[0]);
+  }
+
+  const handleAddClick = () => {
+    setIsOpen(!isOpen);
+  }
+
+  function keyDown(e){
+    console.log(e);
+  }
+
+  function updateTitle(e){
+    setTitle(e.target.value);
   }
 
   useEffect(() => {
@@ -63,6 +60,7 @@ const Editor = () => {
       }
     }
     isLoggedIn();
+    console.log(name.length);
   }, []);
 
   /**
@@ -90,22 +88,39 @@ const Editor = () => {
   }, [accounts]);
 
   return (
-      
     <>
-    {isOpen ?  <Colors onSelectLanguage={handleLanguage}/>  : null}
+    {isOpen ?  <Colors onSelectLanguage={handleLanguage} closePopup={handleAddClick} />  : null}
       <div onClick={togglePopup} className={isOpen ? `content blur` : `content`}>
         <div className={`editor-background`} style={{ backgroundColor: `${colorScheme[0].backgroundColor}` }}>
 
         <div className={"editor-title"} style={{ color: `${colorScheme[0].titleColor}` }}>
-          {name}<i className="tim-icons icon-pencil icon"></i>
+          {/* <EditText name="textbox1" defaultValue="Isabel Battaglioli"/> */}
+    
+          <input
+            className="editor-input"
+            type="text"
+            value={title}
+            maxLength={30}
+            onChange={updateTitle}
+            style={{color:`${colorScheme[0].titleColor}`, backgroundColor:`${colorScheme[0].backgroundColor}`, border:"none", outline: "none"}}
+          />
+          {/* <i className="tim-icons icon-pencil icon" style={{color:"white", margin:"5px"}}></i> */}
+          <Link
+            className="btn btn-primary editor-button"
+            color="primary"
+            onClick={handleAddClick}
+          >
+            Edit Page
+          </Link>
+        
+          
         </div>
           <GridContextProvider onChange={onChange}>
             <GridDropZone
               id="items"
               boxesPerRow={4}
               rowHeight={330}
-              style={{ height: `${(heightSize/4) * 330}px`}} 
-            > 
+              style={{ height: `${(heightSize/4) * 330}px`}}> 
               {Object.values(claimedAccounts).map(item => (
                 <GridItem className="boxes" key={item.id}> 
                   <div
