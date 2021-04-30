c8a01a3ddd4696950662531f7d40fbe41d83ba1c
Jack McKernan <jmcker@outlook.com>
2021-04-30 05:22:52 -0400

    Add 'Now Playing' for in progress searches

---
+++ src/components/NowPlayingSearch/NowPlayinSearch.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { Card, CardBody } from "reactstrap";
+import { Link } from "react-router-dom";
+import { Search, SearchState, ThirdPartyAccount } from "trace-search";
+
+function NowPlayingSearch() {
+  const [currentSearch, setCurrentSearch] = useState(null);
+  const [, setPlsRender] = useState(false);
+
+  const getInProgressSearches = () => {
+    return Object.values(Search.cache.items)
+      .filter(search => search.state === SearchState.IN_PROGRESS)
+      .sort((a, b) => a.id.localeCompare(b.id));
+  }
+
+  useEffect(() => {
+    const triggerRender = () => setPlsRender(prev => !prev);
+    const pickSearch = () => {
+      const searches = getInProgressSearches();
+      console.log(searches);
+      if (searches.length > 0) {
+        setCurrentSearch(searches[0]);
+      }
+      triggerRender();
+    }
+
+    Search.cache.events.on('change', pickSearch);
+    ThirdPartyAccount.resultCache.events.on('change', triggerRender);
+    const interval = setInterval(triggerRender, 60 * 1000);
+    return () => {
+      Search.cache.events.removeListener('change', pickSearch);
+      ThirdPartyAccount.resultCache.events.removeListener('change', triggerRender);
+      clearInterval(interval);
+    }
+  }, []);
+
+  const onSearchPage = window.location.href.includes('/search');
+
+  return (
+    <div style={{ position: 'absolute', width: '100%', bottom: '0', margin: 'auto' }}>
+      {!onSearchPage && getInProgressSearches().slice(0, 2).map(search => (
+        <div style={{ width: '90%', margin: 'auto' }}>
+          <Link to={`/search#${search.id}`} style={{ color: 'inherit' }}>
+            <Card>
+              <CardBody>
+                <h4>{search.definition.name}</h4>
+                <div>State: {search.state}</div>
+                <div>Elapsed: {Math.round((new Date() - search.startedAt) / 1000 / 60)} minutes</div>
+                <div>Results: {search.results.length}</div>
+              </CardBody>
+            </Card>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default NowPlayingSearch;
